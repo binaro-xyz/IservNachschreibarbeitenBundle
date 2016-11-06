13b79dffb487f9ec6f5a1ec6bfce38e3743a0982
Lorenz Sieben <me@lorenz-sieben.com>
2016-11-06 22:34:36 +0100

    Working Copy

--- Controller/NachschreibarbeitenController.php
+++ Controller/NachschreibarbeitenController.php
@@ -3,6 +3,8 @@
 namespace IServ\NachschreibarbeitenBundle\Controller;
 
 use IServ\CoreBundle\Controller\PageController;
+use IServ\CoreBundle\Entity\User;
+use IServ\NachschreibarbeitenBundle\Entity\NachschreibarbeitenDate;
 use IServ\NachschreibarbeitenBundle\Entity\NachschreibarbeitenEntry;
 use IServ\NachschreibarbeitenBundle\Security\Privilege;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -24,11 +26,27 @@ class NachschreibarbeitenController extends PageController {
      * @return array|RedirectResponse
      */
     public function indexAction() {
-      if(!$this->isGranted(Privilege::ACCESS_NACHSCHREIBARBEITEN)) {
-        throw $this->createAccessDeniedException("You are not allowed to view this page.");
-      } else {
-        $repo = $this->getDoctrine()->getManager()->getRepository('IServNachschreibarbeitenBundle:NachschreibarbeitenEntry');
-        return array('dummy' => $repo, 'exercises' => array());
-      }
+        if(!$this->isGranted(Privilege::ACCESS_NACHSCHREIBARBEITEN)) {
+            throw $this->createAccessDeniedException("You are not allowed to view this page.");
+        } else {
+            $repo = $this->getDoctrine()->getManager()->getRepository('IServNachschreibarbeitenBundle:NachschreibarbeitenDate');
+
+//            $nd = new NachschreibarbeitenEntry();
+//            $nd->setDate($repo->find(1));
+//            $nd->setOwner($this->getUser());
+//            $nd->setAdditionalMaterial('');
+//            $nd->setClass('05Gpi');
+//            $nd->setDuration(3141);
+//            $nd->setStudent($this->getUser());
+//            $nd->setSubject('Pi');
+//            $nd->setTeacher($this->getUser());
+//
+//
+//            $this->getDoctrine()->getManager()->persist($nd);
+//            $this->getDoctrine()->getManager()->flush();
+
+            $anderes_repo = $this->getDoctrine()->getManager()->getRepository('IServNachschreibarbeitenBundle:NachschreibarbeitenEntry');
+            return array('dummy' => $repo->findAll(), 'anderes_repo' => $anderes_repo->findAll(), 'exercises' => array());
+        }
     }
 }
