59313680f0adfe1fb027cee83a436338e744abab
Lorenz Sieben <me@lorenz-sieben.com>
2016-11-28 00:30:34 +0100

    Added idesk sidebar widget

--- EventListener/IDeskListener.php
+++ EventListener/IDeskListener.php
@@ -7,42 +7,37 @@ use IServ\CoreBundle\Event\IDeskEvent;
 use IServ\CoreBundle\EventListener\IDeskListenerInterface;
 use IServ\NachschreibarbeitenBundle\Security\Privilege;
 
+
 class IDeskListener implements IDeskListenerInterface
 {
-    private $doctrine;
+    private $manager;
 
     /**
      * @param \Doctrine\Bundle\DoctrineBundle\Registry $doctrine
      */
-    public function __construct(Doctrine $doctrine)
-    {
-        $this->doctrine = $doctrine->getManager();
+    public function __construct(Doctrine $doctrine) {
+        $this->manager = $doctrine->getManager();
+        define('KING_SPOT', -PHP_INT_MAX+(int)3.141592653589793238462643383279502884);
     }
 
     /**
      * @param \IServ\CoreBundle\Event\IDeskEvent $event
      */
-    public function onBuildIDesk(IDeskEvent $event)
-    {
-        if( $event->getAuthorizationChecker()->isGranted(Privilege::EXCLUDED_FROM_EXERCISES)) {
-            return;
-        }
+    public function onBuildIDesk(IDeskEvent $event) {
+        $repo = $this->manager->getRepository('IServNachschreibarbeitenBundle:NachschreibarbeitenEntry');
+        $query = $repo->createQueryBuilder('e')
+            ->join('e.date', 'd', 'WITH', 'd.date >= CURRENT_DATE()')
+            ->orderBy('d.date', 'ASC')
+            ->where('e.student = ?1')
+            ->setParameter(1, $event->getUser()->getId())
+            ->getQuery();
+        $entries = $query->getResult();
 
-        // Get upcoming exercises
-        $limit = 3;
-        $exercises = $this->doctrine->getRepository('IServExerciseBundle:Exercise')->findUpcomingExercisesForUser($event->getUser(), $limit);
-
-        // Inject into IDesk
-        if ($exercises) {
-            $event->addContent(
-                'exercises.upcoming',
-                'IServExerciseBundle::idesk.html.twig',
-                array(
-                    'exercises' => $exercises,
-                    'limit' => $limit,
-                ),
-                -1
-            );
+        if ($entries) {
+            $event->addSidebarContent(
+                'nachschreibarbeiten.upcoming',
+                'IServNachschreibarbeitenBundle::idesk.html.twig',
+                array('entries' => $entries), KING_SPOT);
         }
     }
 }
